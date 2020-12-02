bb7439bfd3a5f91d1744a8da18b636ced8d5e1ee
Alex Uhling <alex.uhling@jefferson.kyschools.us>
2020-12-01 21:00:35 -0500

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			// Part One
+			var input = new List<int>();
+			using var reader = new System.IO.StreamReader("Input.txt");
+			while (reader.Peek() > -1)
+			{
+				var lineItem = await reader.ReadLineAsync();
+
+				if (int.TryParse(lineItem, out var number))
+					input.Add(number);
+			}
+
+			foreach( var numerator in input)
+			{ 
+				if(input.Any(x=>x+numerator == 2020))
+				{
+					var numerator2 = input.First(x => x + numerator == 2020);
+					Console.WriteLine(numerator * numerator2);
+					break;
+				}
+			}
+
+			
+		}
+
+
+	}
+}
