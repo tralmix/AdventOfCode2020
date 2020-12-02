067d586a7f34a0eaf0ad613ecabdc9f6b3fa8714
Alex Uhling <alex.uhling@jefferson.kyschools.us>
2020-12-01 21:20:04 -0500

    Implemented Day01 part 2 solution

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -7,9 +7,13 @@ namespace Day01
 {
 	public class Program
 	{
+		/// <summary>
+		/// I know this is done horribly. Oh well.
+		/// </summary>
 		public static async Task Main(string[] args)
 		{
 			// Part One
+
 			var input = new List<int>();
 			using var reader = new System.IO.StreamReader("Input.txt");
 			while (reader.Peek() > -1)
@@ -20,9 +24,9 @@ namespace Day01
 					input.Add(number);
 			}
 
-			foreach( var numerator in input)
-			{ 
-				if(input.Any(x=>x+numerator == 2020))
+			foreach (var numerator in input)
+			{
+				if (input.Any(x => x + numerator == 2020))
 				{
 					var numerator2 = input.First(x => x + numerator == 2020);
 					Console.WriteLine(numerator * numerator2);
@@ -30,7 +34,25 @@ namespace Day01
 				}
 			}
 
-			
+			// Part Two
+			var found = false;
+			foreach (var numerator1 in input)
+			{
+				if (found) break;
+				foreach (var numerator2 in input.Except(new List<int>() { numerator1 }))
+				{
+					if (input.Any(x => x + numerator1 + numerator2 == 2020))
+					{
+						var numerator3 = input.First(x => x + numerator1 + numerator2 == 2020);
+						Console.WriteLine(numerator1 * numerator2 * numerator3);
+						found = true;
+						break;
+					}
+				}
+			}
+
+
+
 		}
 
 
