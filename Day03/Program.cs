cb3a49cb3091429e98fb0e6bcd4cf2d9c68e8646
Alex Uhling <alex.uhling@jefferson.kyschools.us>
2020-12-03 11:30:43 -0500

    Day3 part 1

---
+++ Day03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day03
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			// Part One
+
+			var inputLines = new List<string>();
+			using var reader = new System.IO.StreamReader("Input.txt");
+			while (reader.Peek() > -1)
+			{
+				var lineItem = await reader.ReadLineAsync();
+
+				inputLines.Add(lineItem);
+			}
+
+			var treeLines = GetExpandedTreeLines(inputLines);
+
+			int treeCount = 0, rightIndex = 0, downIndex = 0;
+
+			do
+			{
+				if (treeLines[downIndex][rightIndex].Equals('#'))
+					treeCount++;
+
+				rightIndex += 3;
+				downIndex += 1;
+
+			} while (downIndex < treeLines.Count());
+
+			Console.WriteLine($"Tree count is {treeCount}");
+		}
+
+		private static List<List<char>> GetExpandedTreeLines(List<string> inputLines)
+		{
+			var treeLines = inputLines.Select(x => x.ToArray().ToList()).ToList();
+			while (treeLines[0].Count() < 3* treeLines.Count())
+			{
+				foreach (var line in treeLines)
+					line.AddRange(line);
+			}
+
+			return treeLines;
+		}
+	}
+}
