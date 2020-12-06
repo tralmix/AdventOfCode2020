d0b59581b2a5c0b7582ed5ce02db5ac88440cb4c
Alex Uhling <alex.uhling@jefferson.kyschools.us>
2020-12-06 09:15:42 -0500

    Day 6 part 2

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -40,6 +40,38 @@ namespace Day06
 			var totalYesAnswers = groupAnswers.Aggregate(0, (count, next) => count + next.ToArray().Distinct().Count());
 
 			Console.WriteLine($"Total yes answer are {totalYesAnswers}");
+
+			groupAnswers = new List<string>();
+			var collectiveYes = new List<char>();
+
+			var firstOfGroup = true;
+			foreach (var input in inputLines)
+			{
+				if (string.IsNullOrWhiteSpace(input))
+				{
+					if(collectiveYes.Count > 0)
+						groupAnswers.Add(string.Join(string.Empty, collectiveYes));
+					collectiveYes = new List<char>();
+					firstOfGroup = true;
+					continue;
+				}
+
+				if (firstOfGroup)
+				{
+					collectiveYes.AddRange(input.ToArray());
+					firstOfGroup = false;
+				}
+				else collectiveYes = collectiveYes.Where(y => input.ToArray().Contains(y)).ToList();
+			}
+
+			// add last answer since no empty line trails it and for loop won't add it.
+			groupAnswers.Add(string.Join(string.Empty, collectiveYes));
+
+			totalYesAnswers = groupAnswers.Aggregate(0, (count, next) => count + next.ToArray().Count());
+
+			Console.WriteLine($"Total yes answer are {totalYesAnswers}");
+
+
 		}
 	}
 }
