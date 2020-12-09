6ac280284c84e389074d70f14b84e8d57ebe4548
Alex Uhling <alex.uhling@jefferson.kyschools.us>
2020-12-09 09:56:42 -0500

    Day 09 part 1

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Day09
@@ -8,7 +9,29 @@ namespace Day09
 	{
 		static async Task Main(string[] args)
 		{
+			const int preamble = 25;
 			var inputLines = await ReadInputLines();
+
+			var inputAsIntegers = inputLines.Select(long.Parse).ToList();
+
+			// Part One
+			int index = FindInvalidIndex(preamble, inputAsIntegers);
+
+			Console.WriteLine($"First number to fail is {inputAsIntegers[index]}");
+		}
+
+		private static int FindInvalidIndex(int preamble, List<long> inputAsIntegers)
+		{
+			var working = true;
+			var index = preamble;
+			do
+			{
+				if (!inputAsIntegers[index].IsSum(inputAsIntegers.GetRange(index - preamble, preamble)))
+					working = false;
+				else
+					index++;
+			} while (index < inputAsIntegers.Count && working);
+			return index;
 		}
 
 		private static async Task<List<string>> ReadInputLines()
@@ -25,5 +48,16 @@ namespace Day09
 
 			return inputLines;
 		}
+	}
+
+	public static class IntExtension
+	{
+		public static bool IsSum(this long value, List<long> options)
+		{
+			if (options.Any(x => options.Any(y => x != y && x + y == value)))
+				return true;
+			return false;
+		}
+
 	}
 }
