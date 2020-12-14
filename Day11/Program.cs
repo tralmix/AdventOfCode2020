0395d5d23995b114e2b5844dc8f96f4a13ddcb17
Alex Uhling <christian.a.uhling@gmail.com>
2020-12-13 19:27:51 -0500

    Day 11 set up

---
+++ Day11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var inputLines = await ReadInputLines();
+		}
+
+		private static async Task<List<string>> ReadInputLines()
+		{
+			var inputLines = new List<string>();
+
+			using var reader = new System.IO.StreamReader("Input.txt");
+			while (reader.Peek() > -1)
+			{
+				var lineItem = await reader.ReadLineAsync();
+
+				inputLines.Add(lineItem);
+			}
+
+			return inputLines;
+		}
+	}
+}
